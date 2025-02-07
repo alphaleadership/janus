361b41af620d83b479f6593107b5dde32e06e63c
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-02-07 14:00:28 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -13,26 +13,27 @@ logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == "__main__":
-    prompt = input('prompt: ')
-    log.info(f'prompt={prompt}')
-    t0 = time.time()
-    pipe = JanusPipeline(
-        repo_id,
-        cache_dir=cache_dir,
-        dtype=torch.bfloat16,
-        device='cuda',
-    )
-    t1 = time.time()
-    log.info(f'load time={t1-t0}')
-    images = pipe(
-        prompt=prompt,
-        num_images_per_prompt=1,
-        temperature=1.0,
-        guidance_scale=5.0,
-    )
-    t2 = time.time()
-    log.info(f'generate time={t2-t1}')
-    for i, image in enumerate(images):
-        fn = f'/tmp/janus-{i}.png'
-        log.info(f'image={i} file={fn}')
-        image.save(fn)
+    
+    def gen(prompt):
+        log.info(f'prompt={prompt}')
+        t0 = time.time()
+        pipe = JanusPipeline(
+            repo_id,
+            cache_dir=cache_dir,
+            dtype=torch.bfloat16,
+            device='cuda',
+        )
+        t1 = time.time()
+        log.info(f'load time={t1-t0}')
+        images = pipe(
+                prompt=prompt,
+            num_images_per_prompt=1,
+            temperature=1.0,
+            guidance_scale=5.0,
+        )
+        t2 = time.time()
+        log.info(f'generate time={t2-t1}')
+        for i, image in enumerate(images):
+            fn = f'./tmp/janus-{i}.png'
+            log.info(f'image={i} file={fn}')
+            image.save(fn)
